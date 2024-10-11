f00315ed4764045b77bbc69ce5f2490637ab65e1
Mehant Kumar <mehant98@gmail.com>
2024-10-11 13:53:26 -0700

    first commit

---
+++ src/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// Yash     
+    /// <summary>
+    /// Sai Sreyas
+    /// <summary>
+    /// Mehant
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        //Hi Mike
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        public void OnGet()
+        {   
+            Products = ProductService.GetAllData();
+        }
+    }
+}
